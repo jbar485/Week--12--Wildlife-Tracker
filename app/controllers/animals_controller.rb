a2fe5b99f0b568f5dbc90faa656a28e1bfcdb1cb
Student <example@example.com>
2020-01-13 15:24:39 -0800

    animals CRUD

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,31 +1,54 @@
 class AnimalsController < ApplicationController
 
-  # Code for listing all albums
+  # Code for listing all animals
   def index
+    @animals = Animal.all
+    render :index
   end
 
-  # Code for new album form
+  # Code for new animal form
   def new
+    @animal = Animal.new
+    render :new
   end
 
-  # Code for creating a new album
+  # Code for creating a new animal
   def create
+    @animal = Animal.new(animal_params)
+    if @animal.save
+      redirecto_to animals_path
+    else
+      render :new
+    end
   end
 
-  # Code for edit album form
+  # Code for edit animal form
   def edit
+    @animal = Animal.find(params[:id])
+    render :edit
   end
 
-  # Code for showing a single album
+  # Code for showing a single animal
   def show
+    @animal = Animal.find(params[:id])
+    render :show
   end
 
-  # Code for updating an album
+  # Code for updating an animal
   def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirecto_to animals_path
+    else
+      render :edit
+    end
   end
 
-  # Code for deleting an album
+  # Code for deleting an animal
   def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+    redirecto_to animals_path
   end
 
   private
